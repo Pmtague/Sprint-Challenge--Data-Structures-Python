795fb7a53919bf4bd4ad1d93343809d298dd6c66
Penny Tague <pmtague9@gmail.com>
2020-02-03 05:19:37 -0800

    More work on ring buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -4,22 +4,38 @@ from doubly_linked_list import DoublyLinkedList
 class RingBuffer:
     def __init__(self, capacity):
         self.capacity = capacity
-        self.current = None
+        self.oldest = None
         self.storage = DoublyLinkedList()
 
     def append(self, item):
         # Is the ring buffer at capacity?
-		# If yes:
-		# 	Store the value of the current head (oldest)
-		#   Overwrite the current head with the newest value
-		#   
-		# If no, add the item to the next available slot
+        # If yes:
+        if self.capacity == self.storage.length:
+            # 	Store the value of the current head (oldest)
+            curr_val = self.storage.head.value
+            #   Overwrite the current head with the newest value
+            self.storage.head = item
+            #   Move the oldest marker to the next element
+            self.oldest = self.storage.head.next
+        # If no, add the item to the next available slot
+        else:
+            self.storage.add_to_tail(item)
 
     def get(self):
         # Note:  This is the only [] allowed
         list_buffer_contents = []
 
-        # TODO: Your code here
+        if self.storage.head == None:
+            return None
+
+        if self.storage.head == self.storage.tail:
+            list_buffer_contents.append(self.storage.head.value)
+
+        else:
+            current = self.storage.head
+            while current != None:
+                list_buffer_contents.append(current.value)
+                current = current.next
 
         return list_buffer_contents
 
